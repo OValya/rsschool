60d942e40dba8d32baa0d5812cce9f1375b5c410
OValya <valya.eto.ya@gmail.com>
2021-10-01 01:21:38 +0300

    feat: add video section

--- museum/js/script.js
+++ museum/js/script.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
-    $('.slick').slick();
+//$(document).ready(function(){
+   // $('.slick').slick();
+//});
 
-});
+let progressVolume = document.querySelector('.progress-volume'); 
+progressVolume.addEventListener('input', function() {
+  let value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
+ 
+let progressVideo = document.querySelector('.progress-video'); 
+progressVideo.addEventListener('input', function() {
+  let value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
+})
