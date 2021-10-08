6c9d8a74c66540268efee7ca37840876736a9793
OValya <valya.eto.ya@gmail.com>
2021-10-08 16:57:19 +0300

    feat: add slider on arrows

--- museum/js/script.js
+++ museum/js/script.js
@@ -1,6 +1,29 @@
-//$(document).ready(function(){
-   // $('.slick').slick();
-//});
+const slidePrev = document.querySelector('.slide-prev');
+const slideNext = document.querySelector('.slide-next');
+const containerWelcome = document.querySelector('.welcome-container')
+let numberWelcomeImage = 1;
+const slideCounter = document.querySelector('.slide-number-counter')
+
+function setNumberSlider(){
+  slideCounter.textContent = String(numberWelcomeImage).padStart(2, '0');
+}
+
+function setWelcomeImage(){
+  containerWelcome.style.background = `linear-gradient(to left, transparent 30%, #030303 70%), url('/assets/img/welcome/${numberWelcomeImage}.jpg') 100% 0% no-repeat`
+}
+setWelcomeImage();
+
+slidePrev.onclick = () => {
+  (numberWelcomeImage===1)?numberWelcomeImage=5:numberWelcomeImage--;
+  setWelcomeImage();
+  setNumberSlider();
+}
+
+slideNext.onclick = () => {
+  (numberWelcomeImage===5)?numberWelcomeImage=1:numberWelcomeImage++;
+  setWelcomeImage();
+  setNumberSlider();
+}
 
 let progressVolume = document.querySelector('.progress-volume'); 
 progressVolume.addEventListener('input', function() {
