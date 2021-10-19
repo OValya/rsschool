2d570a2d33915f9519e2f6c312f93e8a2bee5ed2
OValya <valya.eto.ya@gmail.com>
2021-10-19 13:46:03 +0300

    feat: add translate En-Ru

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -20,7 +20,12 @@ const PLAY = document.querySelector('.play');
 const btnPlayPREV = document.querySelector('.play-prev')
 const btnPlayNEXT = document.querySelector('.play-next')
 let numPlay = 0;
-const PLAYLIST = document.querySelector('.play-list')
+const PLAYLIST = document.querySelector('.play-list');
+const LANGUAGE = document.querySelector('.language');
+let isRussian = true;
+
+
+//----------------дата-время-----------------
 
 function showTime(){
     const date = new Date();
@@ -32,9 +37,11 @@ function showTime(){
 }
 
 function showDate(){
+    let local;
     const date = new Date();
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
-    const local = 'ru-RU';
+    if(isRussian) local = 'ru-RU';
+    else local = 'en-EN';
     const currentDate = date.toLocaleDateString(local, options);
     DATE.textContent = currentDate;
 }
@@ -44,12 +51,33 @@ showTime();
 function getTimeOfDay(){
     const date = new Date();
     const hour = date.getHours();
-    const massiveTimes = ['night', 'morning', 'day', 'evening']
+    const massiveTimes = ['night', 'morning', 'afternoon', 'evening']
     return massiveTimes[Math.floor(hour/6)];
 }
 
+
+//-----------приветствие-------------------
+
 function showGreeting(){
-    GREETING.textContent = `Good ${getTimeOfDay()}, `;
+    if(!isRussian) {GREETING.textContent = `Good ${getTimeOfDay()}, `;}
+    else{
+        switch (getTimeOfDay()) {
+            case 'night':
+                GREETING.textContent = 'Доброй ночи, ';
+                break;
+            case 'afternoon':
+                GREETING.textContent = 'Добрый день, ';
+                break;
+            case 'morning':
+                GREETING.textContent = 'Доброе утро, ';
+                break;
+            case 'evening':
+                GREETING.textContent = 'Добрый вечер, ';
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 function setLocalStorage(){
@@ -70,6 +98,10 @@ function getLocalStorage(){
 
 window.addEventListener('load', getLocalStorage);
 
+
+//---------------фон--------------------------
+
+
 function getRandomInt() {
     randomNum = Math.ceil(Math.random() * 20); 
   }
@@ -80,6 +112,7 @@ function changeBackground(){
     const timeOfDay = getTimeOfDay();
     const numberImg = String(randomNum).padStart(2,'0');
     const img = new Image();
+    //BODY.style.backgroundColor = "#000000"
     img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${numberImg}.jpg`;
     img.onload = ()=>{
         BODY.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${numberImg}.jpg')`;
@@ -101,8 +134,14 @@ function getSlidePrev(){
 btnSlideNext.addEventListener('click', getSlideNext);
 btnSlidePrev.addEventListener('click', getSlidePrev);
 
+
+//-----------------погода----------------------
+
 async function getWeather() {  
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=92be861a33f2edd35d1a39fd6838b1a2&units=metric&lang=ru`;
+    let languageOption;
+    if(isRussian) {languageOption='ru'}
+    else{languageOption='en'}
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=92be861a33f2edd35d1a39fd6838b1a2&units=metric&lang=${languageOption}`;
     const res = await fetch(url);
     const data = await res.json(); 
     //console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
@@ -110,14 +149,21 @@ async function getWeather() {
     weatherDescription.textContent = data.weather[0].description;
     weatherIcon.className = 'weather-icon owf';
     weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+    //wind
+    //humidity
 }
 
 city.addEventListener('change', getWeather);
 getWeather()
 
+
+//-------------цитаты-------------
+
 async function getQuote(){
-    const src = 'js/quotes.json';
-    const res = await fetch(src);
+    let srcQuete;
+    if(isRussian) {srcQuete  = 'js/quotes_RU.json';}
+    else {srcQuete = 'js/quotes_En.json'}
+    const res = await fetch(srcQuete);
     const quotes = await res.json();
     let randomQuote = Math.ceil(Math.random() * 11);
     //console.log(randomQuote);
@@ -129,7 +175,9 @@ window.addEventListener('load', getQuote);
 
 changeQUOTE.addEventListener('click', getQuote);
 
+
 //-----------audio--------
+
 function playAudio(){
    AUDIO.currentTime = 0;
    if(isPlay){AUDIO.pause(); isPlay = false; PLAY.classList.remove('pause')}
@@ -173,7 +221,19 @@ playList.forEach(el => {
     PLAYLIST.append(LI);
 })
 
+//-------change language----------
+LANGUAGE.addEventListener('click', changeLanguage);
 
+function changeLanguage(){
+    if(isRussian){isRussian=false; LANGUAGE.value='RU'}
+    else {isRussian=true;LANGUAGE.value='EN'}
+    showGreeting();
+    showDate();
+    getQuote();
+    getWeather();
+    
+    
+}
 
 //------------------------
 
