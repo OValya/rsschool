7d4dadc2bce0555192d5c3fc86f822bbdfaf876b
OValya <valya.eto.ya@gmail.com>
2021-11-14 15:05:13 +0300

    feat: add start page layout

---
+++ art-quiz/script.js
@@ -0,0 +1,9 @@
+const iconSettings = document.querySelector('.settings-icon');
+const containerSettings = document.querySelector('.settings-page');
+
+iconSettings.addEventListener('click', (e) => {
+   // console.log('gjkhlkj;jkg');
+    containerSettings.classList.toggle('_show');
+    //setOptionVisibility();
+    
+});
