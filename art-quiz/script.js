d5d5a313f0850f5569644a7ee9372d0a1fa25a6e
OValya <valya.eto.ya@gmail.com>
2021-11-14 18:59:02 +0300

    feat: add settings page layout

--- art-quiz/script.js
+++ art-quiz/script.js
@@ -1,5 +1,9 @@
 const iconSettings = document.querySelector('.settings-icon');
 const containerSettings = document.querySelector('.settings-page');
+const minusTime = document.querySelector('.minus');
+const plusTime = document.querySelector('.plus');
+const timerValue = document.querySelector('.timer-value');
+
 
 iconSettings.addEventListener('click', (e) => {
    // console.log('gjkhlkj;jkg');
@@ -7,3 +11,15 @@ iconSettings.addEventListener('click', (e) => {
     //setOptionVisibility();
     
 });
+
+
+
+minusTime.addEventListener('click', (e) => {
+    timerValue.stepDown(); 
+    //timerValue.onchange();
+})
+
+plusTime.addEventListener('click', (e)=>{
+    timerValue.stepUp(); 
+    //timerValue.onchange();
+})
