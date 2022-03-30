06ae8a4b50ada84a530c6ac5bc30354a50cb923d
OValya <valya.eto.ya@gmail.com>
2022-03-30 13:28:30 +0300

    feat: add classsearch

---
+++ react-component/src/components/ClassCards.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+type Props = {}
+
+type State = {}
+
+export default class ClassCards extends Component<Props, State> {
+  state = {}
+
+  render() {
+    return (
+
+
+      <div>ClassCards</div>
+    )
+  }
+}
+
--- react-component/src/components/ClassSearch.tsx
+++ react-component/src/components/ClassSearch.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 
-export default class ClassSearch extends React.Component{
-  constructor(props:string){
+export default class ClassSearch extends React.Component<{}, {value:string}> {
+  constructor(props: string) {
     super(props);
+    const search = localStorage.getItem('search');
+    const searchString = search ? search : '';
+    this.state = { value: searchString };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {
+    localStorage.setItem('search', this.state.value);
+  }
+
+  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ value: event.target.value });
   }
 
   render(): React.ReactNode {
-      return(
-        <div>
-          <input className="inputSearch" type="text" placeholder="Enter search" id="" />
-          <button className="btnSearch">Search</button>
-        </div>
-      
-      )
-  }
+    return (
+      <div>
+        <input
+          className="inputSearch"
+          type="text"
+          placeholder="Enter search"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
 
+        <button className="btnSearch">Search</button>
+      </div>
+    );
+  }
 }
